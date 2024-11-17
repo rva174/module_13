534eeb921ca44220c1c9824eae7216e620280dcd
rva174 <rva174@yandex.ru>
2024-11-17 12:14:48 +0500

    init

--- module_13_2.py
+++ module_13_2.py
@@ -7,6 +7,7 @@ api = ' '
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
+
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     print("Привет! Я бот, помогающий твоему здоровью.")
