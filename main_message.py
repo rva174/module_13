94733e75b88c4f17aadbbdcc3284f558189beeb0
rva174 <rva174@yandex.ru>
2024-11-17 14:17:30 +0500

    init

--- main_message.py
+++ main_message.py
@@ -8,18 +8,20 @@ bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
 
-
 @dp.message_handler(text=["Urban", "ff"])
 async def urban_message(message):
     print("Urban message")
+    await message.answer("Urban message")
 
 @dp.message_handler(commands=["start"])
 async def start_message(message):
     print("Start message")
+    await message.answer("Рады вас видеть в нашем блоге!")
 
 @dp.message_handler()
-async def all_messages(message):
+async def all_message(message):
     print("Мы получили сообщение")
+    await message.answer(message.text.upper())
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
